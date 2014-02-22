adb07e18e89f715fe15972a8f29cf00987a78a81
seikichi <seikichi@kmc.gr.jp>
2014-02-23 06:15:52 +0900

    メタデータの各フィールドに対してクラスを作成した

--- shoshi/metadata.py
+++ shoshi/metadata.py
@@ -3,34 +3,69 @@
 
 from collections import namedtuple
 
-Title = namedtuple('Title', ['name', 'transcription',
-                             'parallels', 'related_information'])
-TitleElement = namedtuple('TitleElement', ['name', 'transcription', 'parallels'])
-Publisher = namedtuple('Publisher', ['name', 'transcription', 'location'])
-Series = namedtuple('Series', ['title', 'number'])
-Volume = namedtuple('Volume', ['name', 'transcription'])
-Creator = namedtuple('Creator', ['name', 'transcription', 'role',
-                                 'date_of_birth', 'date_of_death'])
-Content = namedtuple('Content', ['title', 'creators'])
-_Metadata = namedtuple('Metadata', [
-    'title',
-    'volume',
-    'series',
-    'publishers',
-    'creators',
-    'contents',
-    'identifiers',
-    'description',
-    'tags',
-    'thumbnails',
-    'price',
-    'published_date',
-    'page_count',
-    'links'
-])
+
+class Title(namedtuple('Title', ['name', 'transcription',
+                                 'parallels', 'related_information'])):
+    def __new__(klass, name, transcription=None,
+                parallels=[], related_information=[]):
+        return super(Title, klass).__new__(klass, name, transcription,
+                                           parallels, related_information)
 
 
-class Metadata(_Metadata):
+class TitleElement(namedtuple('TitleElement', [
+        'name', 'transcription', 'parallels'])):
+    def __new__(klass, name, transcription=None, parallels=[]):
+        return super(TitleElement, klass).__new__(klass, name, transcription, parallels)
+
+
+class Publisher(namedtuple('Publisher', ['name', 'transcription', 'location'])):
+    def __new__(klass, name, transcription=None, location=None):
+        return super(Publisher, klass).__new__(klass, name, transcription, location)
+
+
+class Volume(namedtuple('Volume', ['name', 'transcription'])):
+    def __new__(klass, name, transcription=None):
+        return super(Volume, klass).__new__(klass, name, transcription)
+
+
+class Creator(namedtuple('Creator', ['name', 'transcription', 'role',
+                                     'date_of_birth', 'date_of_death'])):
+    def __new__(klass,
+                name,
+                transcription=None,
+                role=None,
+                date_of_birth=None,
+                date_of_death=None):
+        return super(Creator, klass).__new__(
+            klass, name, transcription, role,
+            date_of_birth, date_of_death)
+
+
+class Content(namedtuple('Content', ['title', 'creators'])):
+    def __new__(klass, title, creators=[]):
+        return super(Content, klass).__new__(klass, title, creators)
+
+
+class Series(namedtuple('Series', ['title', 'number'])):
+    def __new__(klass, title, number=None):
+        return super(Series, klass).__new__(klass, title, number)
+
+
+class Metadata(namedtuple('Metadata', [
+        'title',
+        'volume',
+        'series',
+        'publishers',
+        'creators',
+        'contents',
+        'identifiers',
+        'description',
+        'tags',
+        'thumbnails',
+        'price',
+        'published_date',
+        'page_count',
+        'links'])):
     def __new__(klass,
                 title=None,
                 volume=None,
