3a22521c3480e0884396b2781d984270637de194
seikichi <seikichi@kmc.gr.jp>
2014-02-23 09:08:33 +0900

    stops to use collections.namedtuple

--- shoshi/metadata.py
+++ shoshi/metadata.py
@@ -1,107 +1,95 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from collections import namedtuple, Mapping
+
+class Title(object):
+    def __init__(self, name, transcription=None,
+                 parallels=[], related_information=[]):
+        self.name = name
+        self.transcription = transcription
+        self.parallels = parallels
+        self.related_information = related_information
 
 
-class Title(namedtuple('Title', ['name', 'transcription',
-                                 'parallels', 'related_information'])):
-    def __new__(klass, name, transcription=None,
-                parallels=[], related_information=[]):
-        return super(Title, klass).__new__(klass, name, transcription,
-                                           parallels, related_information)
+class TitleElement(object):
+    def __init__(self, name, transcription=None, parallels=[]):
+        self.name = name
+        self.transcription = transcription
+        self.parallels = parallels
 
 
-class TitleElement(namedtuple('TitleElement', [
-        'name', 'transcription', 'parallels'])):
-    def __new__(klass, name, transcription=None, parallels=[]):
-        return super(TitleElement, klass).__new__(
-            klass, name, transcription, parallels)
+class Publisher(object):
+    def __init__(self, name, transcription=None, location=None):
+        self.name = name
+        self.transcription = transcription
+        self.location = location
 
 
-class Publisher(namedtuple('Publisher', ['name', 'transcription', 'location'])):
-    def __new__(klass, name, transcription=None, location=None):
-        return super(Publisher, klass).__new__(
-            klass, name, transcription, location)
+class Volume(object):
+    def __init__(self, name, transcription=None):
+        self.name = name
+        self.transcription = transcription
 
 
-class Volume(namedtuple('Volume', ['name', 'transcription'])):
-    def __new__(klass, name, transcription=None):
-        return super(Volume, klass).__new__(klass, name, transcription)
+class Creator(object):
+    def __init__(self,
+                 name,
+                 transcription=None,
+                 role=None,
+                 date_of_birth=None,
+                 date_of_death=None):
+        self.name = name
+        self.transcription = transcription
+        self.role = role
+        self.date_of_birth = date_of_birth
+        self.date_of_death = date_of_death
 
 
-class Creator(namedtuple('Creator', ['name', 'transcription', 'role',
-                                     'date_of_birth', 'date_of_death'])):
-    def __new__(klass,
-                name,
-                transcription=None,
-                role=None,
-                date_of_birth=None,
-                date_of_death=None):
-        return super(Creator, klass).__new__(
-            klass, name, transcription, role,
-            date_of_birth, date_of_death)
+class Content(object):
+    def __init__(self, title, creators=[]):
+        self.title = title
+        self.creators = creators
 
 
-class Content(namedtuple('Content', ['title', 'creators'])):
-    def __new__(klass, title, creators=[]):
-        return super(Content, klass).__new__(klass, title, creators)
+class Series(object):
+    def __init__(self, title, number=None):
+        self.title = title
+        self.number = number
 
 
-class Series(namedtuple('Series', ['title', 'number'])):
-    def __new__(klass, title, number=None):
-        return super(Series, klass).__new__(klass, title, number)
-
-
-class Metadata(namedtuple('Metadata', [
-        'title',
-        'volume',
-        'series',
-        'publishers',
-        'creators',
-        'contents',
-        'identifiers',
-        'description',
-        'tags',
-        'thumbnails',
-        'price',
-        'published_date',
-        'page_count',
-        'links'])):
-    def __new__(klass,
-                title=None,
-                volume=None,
-                series=[],
-                publishers=[],
-                creators=[],
-                contents=[],
-                identifiers={},
-                description=None,
-                tags=[],
-                thumbnails={},
-                price=None,
-                published_date=None,
-                page_count=None,
-                links=[]):
-        return super(Metadata, klass).__new__(
-            klass,
-            title,
-            volume,
-            series,
-            publishers,
-            creators,
-            contents,
-            identifiers,
-            description,
-            tags,
-            thumbnails,
-            price,
-            published_date,
-            page_count,
-            links)
+class Metadata(object):
+    def __init__(self,
+                 title=None,
+                 volume=None,
+                 series=[],
+                 publishers=[],
+                 creators=[],
+                 contents=[],
+                 identifiers={},
+                 description=None,
+                 tags=[],
+                 thumbnails={},
+                 price=None,
+                 published_date=None,
+                 page_count=None,
+                 links=[]):
+        self.title = title
+        self.volume = volume
+        self.series = series
+        self.publishers = publishers
+        self.creators = creators
+        self.contents = contents
+        self.identifiers = identifiers
+        self.description = description
+        self.tags = tags
+        self.thumbnails = thumbnails
+        self.price = price
+        self.published_date = published_date
+        self.page_count = page_count
+        self.links = links
 
     def todict(self, include_none_value_field=True, to_camel_case=True):
-        return namedtuple2dict(self, include_none_value_field, to_camel_case)
+        return object2dict(self, include_none_value_field, to_camel_case)
 
 
 def snake2camel(snake_str):
@@ -109,22 +97,18 @@ def snake2camel(snake_str):
     return components[0] + "".join(x.title() for x in components[1:])
 
 
-def namedtuple2dict(o, include_none_value_field, to_camel_case):
-    if hasattr(o, '_asdict'):
-        return namedtuple2dict(o._asdict(),
-                               include_none_value_field,
-                               to_camel_case)
-    elif isinstance(o, (set, list)):
-        return [namedtuple2dict(item,
-                                include_none_value_field,
-                                to_camel_case)
+def object2dict(o, include_none_value_field, to_camel_case):
+    if hasattr(o, '__dict__'):
+        return object2dict(o.__dict__,
+                           include_none_value_field,
+                           to_camel_case)
+    elif isinstance(o, (list, set)):
+        return [object2dict(item, include_none_value_field, to_camel_case)
                 for item in o]
-    elif isinstance(o, Mapping):
+    elif isinstance(o, dict):
         pairs = []
         for key in o:
-            value = namedtuple2dict(o[key],
-                                    include_none_value_field,
-                                    to_camel_case)
+            value = object2dict(o[key], include_none_value_field, to_camel_case)
             if to_camel_case:
                 key = snake2camel(key)
             pairs.append((key, value))
